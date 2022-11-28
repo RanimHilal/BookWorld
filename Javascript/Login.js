3034f5f56fbe8b456ee0bb9a898ac5457d8ead6d
Afra Aljarwan <u21101560@sharajh.ac.ae>
2022-11-28 22:12:28 +0400

    afra_js

---
+++ Javascript/Login.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementById("check","id");
+  if (!x.checkValidity()) {
+    alert("Must be filled out !");
+} else {
+    alert("Your successfully logged in");
+    
+    } 
+} 
