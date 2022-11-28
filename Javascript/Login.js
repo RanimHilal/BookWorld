1fa5b75c8bf6d251332c5d098fdaf37f1546fe63
AfraAljarwan <113280975+AfraAljarwan@users.noreply.github.com>
2022-11-29 00:42:24 +0400

    Update Login.js

--- Javascript/Login.js
+++ Javascript/Login.js
@@ -1,9 +1,8 @@
 function myFunction() {
-  var x = document.getElementById("check","id");
+  const x = document.getElementById("id1","id2");
   if (!x.checkValidity()) {
-    alert("Must be filled out !");
-} else {
-    alert("Your successfully logged in");
-    
-    } 
+    document.getElementById("demo").innerHTML = x.validationMessage;
+  } else {
+    document.getElementById("demo").innerHTML = "Your successfully logged in";
+  } 
 } 
