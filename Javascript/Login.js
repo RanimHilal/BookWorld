928e270107bb76cf90e081f359d89152689ed351
AfraAljarwan <113280975+AfraAljarwan@users.noreply.github.com>
2022-12-02 15:40:57 +0400

    Update Login.js

--- Javascript/Login.js
+++ Javascript/Login.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function checkFunction() {
   const x = document.getElementById("id1","id2");
   if (!x.checkValidity()) {
     document.getElementById("demo").innerHTML = x.validationMessage;
