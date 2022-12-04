63bdf71ea4a23b76f7beaef820079f59c906dc45
AfraAljarwan <113280975+AfraAljarwan@users.noreply.github.com>
2022-12-04 11:28:40 +0400

    Update Login.js

--- Javascript/Login.js
+++ Javascript/Login.js
@@ -1,4 +1,4 @@
-function checkFunction() {
+function mykFunction() {
   const x = document.getElementById("id1","id2");
   if (!x.checkValidity()) {
     document.getElementById("demo").innerHTML = "Please complete the fields given.";
