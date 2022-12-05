82d2562a3281ac36cd9674197ef21cc10a6fdf68
Hajar-Abdullah <113968614+Hajar-Abdullah@users.noreply.github.com>
2022-12-05 23:44:44 +0400

    Update Rating.js

--- Javascript/Rating.js
+++ Javascript/Rating.js
@@ -1,6 +1,6 @@
          function myFunction(){
-          let data = "";  let name = document.getElementById("userName").value
-          let book = document.getElementById("BookName").value
+          let data = "";  let name = document.getElementById("UserName").value
+          let book = document.getElementById("bookName").value
           let comment = document.getElementById("userComment").value
          
          data = "User name : "+name+"<br/>Book name : "+book+ "<br/>User comment : "+comment
