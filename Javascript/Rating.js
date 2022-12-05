80473ef73c5e046bd90f3cd1d60437172bb20f3f
Hajar-Abdullah <hajar.ab.mo732@outlook.com>
2022-12-05 23:09:17 +0400

    JavaScript file added

---
+++ Javascript/Rating.js
@@ -0,0 +1,9 @@
+         function myFunction(){
+          let data = "";  let name = document.getElementById("userName").value
+          let book = document.getElementById("bookName").value
+          let comment = document.getElementById("userComment").value
+         
+         data = "User name : "+name+"<br/>Book name : "+book+ "<br/>User comment : "+comment
+         
+         document.getElementById("data").innerHTML = data  // display data to paragraph
+         }
