1aaf7488c404975f708d76ca91dcb8ba2839f0a8
Hajar-Abdullah <113968614+Hajar-Abdullah@users.noreply.github.com>
2022-12-06 10:56:49 +0400

    Update Sign_Up.js

--- Javascript/Sign_Up.js
+++ Javascript/Sign_Up.js
@@ -2,7 +2,3 @@ document.getElementById("myInput").oninvalid = function() {myFunction()};
 function myFunction() {
   alert("You must fill out the form!");
 }
-
-function myFunction() {
-  .alert("You must fill out the form of the sign up page!");
-}
